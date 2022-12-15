c81c6f749b4ae28714270e9be36b1b6a556f3275
BoGyeong1 <wls6590@gmail.com>
2022-12-15 15:21:14 +0900

    UP dataInfor

--- src/main/java/com/yojulab/study_servlets/SearchFormServlet.java
+++ src/main/java/com/yojulab/study_servlets/SearchFormServlet.java
@@ -2,6 +2,7 @@ package com.yojulab.study_servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,12 +13,22 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SearchFormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 문자표 인식
+        response.setContentType("text/html;charset=UTF-8");
+        // 출력
         PrintWriter printWriter = response.getWriter();
-        String title = "Search";
+
+        // 데이터 받아오기
+        DatasInfor datasInfor = new DatasInfor();
+        HashMap<String, String> searchFormData = datasInfor.getSearchFormData();
+
+        // HTML 구현
+        String title = searchFormData.get("search_key");
         printWriter.println("<!DOCTYPE html>");
         printWriter.println("<html lang='en'>");
         printWriter.println("<head>");
-
+        // 명시적으로 utf 8 정해줘야
+        printWriter.println("<meta charset='UTF-8' />");
         printWriter.println("    <title>" + title + "</title>");
         printWriter.println("    <link");
         printWriter.println("      href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'");
@@ -32,7 +43,7 @@ public class SearchFormServlet extends HttpServlet {
         printWriter.println("  <body>");
         printWriter.println("    <form action=''>");
         printWriter.println("      <div class='container'>");
-        printWriter.println("        <div class='fs-3'>Search Form</div>");
+        printWriter.println("        <div class='fs-3'>Search Form-" + searchFormData.get("name") + "</div>");
         printWriter.println("        <div>");
         printWriter.println("          <label for='' class='form-label'>Search with Name</label>");
         printWriter.println("          <input");
