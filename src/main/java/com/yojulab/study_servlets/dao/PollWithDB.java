27da4fa9ffafa01b939a2eb4823d2a4706ce98c6
BoGyeong1 <wls6590@gmail.com>
2022-12-22 11:11:51 +0900

    UP poll

---
+++ src/main/java/com/yojulab/study_servlets/dao/PollWithDB.java
@@ -0,0 +1,30 @@
+package com.yojulab.study_servlets.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+public class PollWithDB {
+
+    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException {
+
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+
+        String query = "SELECT * FROM QUESTIONS_LIST " +
+                " WHERE QUESTIONS_UID='" + questionsUid + "'";
+
+        ResultSet reseltSet = statement.executeQuery(query);
+        HashMap<String, Object> result = null;
+        while (reseltSet.next()) {
+            result = new HashMap<>();
+            result.put("QUESTIONS_UID", reseltSet.getString("QUESTIONS_UID"));
+            result.put("QUESTIONS", reseltSet.getString("QUESTIONS"));
+            result.put("ORDERS", reseltSet.getInt("ORDERS"));
+
+        }
+
+        return result;
+    }
+}
